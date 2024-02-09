ae99b08128d0ef445dab245eb9837a820fd79b12
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2024-02-09 03:29:03 -0800

    🛠️refactor(eslintrc) update ESLint configuration ⚙️chore(dependencies) add nativewind and upgrade related packages 🛠️refactor(app) leverage nativewind for styled components in App.tsx ⚙️chore(vscode) adjust eslint settings in vscode config 🛠️refactor(babel) remove dangling commas in babel config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,20 @@
 module.exports = {
   root: true,
   extends: [
-    "universe/native",
-    "plugin:prettier/recommended", // Integrates Prettier with ESLint
+    'universe/native',
+    'plugin:prettier/recommended', // Integrates Prettier with ESLint
   ],
+  plugins: ['prettier'],
   rules: {
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.tsx'] }],
+    'react/prop-types': 'off', // If using TypeScript, you might want to turn this off
+    'no-unused-vars': 'warn',
+    'prettier/prettier': [
+      'error',
+      { semi: false, singleQuote: true, jsxSingleQuote: true },
+    ], // Ensures code format consistency
     // add your custom rules here
   },
-};
+}
--- App.tsx
+++ App.tsx
@@ -1,59 +1,35 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList, StatusBar } from "react-native";
+import { styled } from 'nativewind'
+import React, { useState } from 'react'
+import { Text, View, FlatList, StatusBar } from 'react-native'
+
+// Styled Components
+const StyledText = styled(Text)
+const StyledView = styled(View)
 
 export default function App() {
   // Sample tasks
   const [tasks, setTasks] = useState([
-    { id: "1", text: "Task 1" },
-    { id: "2", text: "Task 2" },
-    { id: "3", text: "Task 3" },
+    { id: '1', text: 'Task 1' },
+    { id: '2', text: 'Task 2' },
+    { id: '3', text: 'Task 3' },
     // Add more tasks here
-  ]);
+  ])
 
   // Render Task Item
-  const renderTask = ({ item }) => (
-    <View style={styles.taskItem}>
-      <Text style={styles.taskText}>{item.text}</Text>
-    </View>
-  );
+  const renderTask = ({ item }: { item: { id: string; text: string } }) => (
+    <StyledView className='p-2 h-screen bg-blue'>
+      <StyledText className='text-xl'>{item.text}</StyledText>
+    </StyledView>
+  )
 
   return (
-    <View style={styles.container}>
+    <StyledView>
       <FlatList
         data={tasks}
         renderItem={renderTask}
         keyExtractor={(item) => item.id}
-        contentContainerStyle={styles.taskListContainer}
       />
-      <StatusBar style="auto" />
-    </View>
-  );
+      <StatusBar barStyle='default' />
+    </StyledView>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f0f0f0",
-  },
-  taskListContainer: {
-    paddingTop: 20,
-  },
-  taskItem: {
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    backgroundColor: "#ffffff",
-    borderRadius: 5,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.23,
-    shadowRadius: 2.62,
-    elevation: 4,
-  },
-  taskText: {
-    fontSize: 16,
-  },
-});
